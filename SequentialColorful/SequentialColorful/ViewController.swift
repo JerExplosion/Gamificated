333dcd4fde3b54bf54add3960d50d26f2424fd1e
jer .explosion <jerryren.ios@gmail.com>
2021-05-26 19:53:58 -0400

    四块 sequential-colorful .init

---
+++ SequentialColorful/SequentialColorful/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SequentialColorful
+//
+//  Created by Jerry Ren on ~/~/21.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
