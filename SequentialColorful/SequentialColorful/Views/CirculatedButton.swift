9e40ce11fa09aebaabf96cc6b87af945c7373013
jer .explosion <jerryren.ios@gmail.com>
2021-05-27 18:07:55 -0400

    Overriding property isHighlighted in CirculatedButton

--- SequentialColorful/SequentialColorful/Views/CirculatedButton.swift
+++ SequentialColorful/SequentialColorful/Views/CirculatedButton.swift
@@ -12,4 +12,15 @@ class CirculatedButton: UIButton {
         layer.masksToBounds = true
         layer.cornerRadius = frame.size.height / 2
     }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            switch isHighlighted {
+            case true:
+                alpha = 1/1
+            default:
+                alpha = 1/2
+            }
+        }
+    }
 }
