44987fea3a668f4af3a7f60d80c4704daefb93d5
jer .explosion <jerryren.ios@gmail.com>
2021-05-29 21:42:24 -0400

    GameScene: SKScene _init

---
+++ SpriteKiddo/SpriteKiddo/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  SpriteKiddo
+//
+//  Created by Jerry Ren on 5/28/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+
+    }
+}
