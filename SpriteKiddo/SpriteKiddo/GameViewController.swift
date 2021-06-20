46eacfd558029b394915ab9cad11e6d467f194b2
jer .explosion <jerryren.ios@gmail.com>
2021-06-20 17:20:46 -0400

    GameViewController •showsPhysics _init

---
+++ SpriteKiddo/SpriteKiddo/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SpriteKiddo
+//
+//  Created by Jerry Ren on ~/~/21.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            
+            let gamescene = SKScene(size: view.bounds.size)
+            gamescene.scaleMode = .aspectFill
+            
+            view.presentScene(gamescene)
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+            view.showsPhysics = true
+        }
+    }
+}
