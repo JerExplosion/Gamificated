9f86cd87f9bad6e984787f11aec3f4e1f78333b0
jerry .explodes <jerryexplodes@gmail.com>
2021-07-04 11:56:52 -0400

    Initializing and presenting MapTileZero.sks (SKScene)

--- SpriteKiddo/SpriteKiddo/GameViewController.swift
+++ SpriteKiddo/SpriteKiddo/GameViewController.swift
@@ -15,16 +15,18 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             
-            let gamescene = GameSceneTwoPointO(size: view.bounds.size)
-                // GameScene(size: view.bounds.size)
-            gamescene.scaleMode = .aspectFill
-            
-            view.presentScene(gamescene)
-            
-            view.ignoresSiblingOrder = true
-            view.showsFPS = true
-            view.showsNodeCount = true
-            view.showsPhysics = true
+            if let gamescene = SKScene(fileNamed: "MapTileZero") {
+                // let gamescene = GameSceneTwoPointO(size: view.bounds.size)
+                // let gamescene = GameScene(size: view.bounds.size)
+                gamescene.scaleMode = .aspectFill
+                
+                view.presentScene(gamescene)
+                
+                view.ignoresSiblingOrder = true
+                view.showsFPS = true
+                view.showsNodeCount = true
+                view.showsPhysics = true
+            }
         }
     }
 }
