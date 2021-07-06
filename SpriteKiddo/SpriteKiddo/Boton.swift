a53e8eef819f7e8153f4fa16c45a5a56a579e28d
jer .explosion <jerryren.ios@gmail.com>
2021-07-05 21:25:58 -0400

    TouchesEnded - TouchesMoved - TouchesBegan - TouchesCancelled

--- SpriteKiddo/SpriteKiddo/Boton.swift
+++ SpriteKiddo/SpriteKiddo/Boton.swift
@@ -27,4 +27,35 @@ class Boton: SKSpriteNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        defaultBoton.alpha = 0.7
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let firstTouch = touches.first else { return }
+        let touch: UITouch = firstTouch
+        let contactPosition: CGPoint = touch.location(in: self)
+        
+        if defaultBoton.contains(contactPosition) {
+            defaultBoton.alpha = 0.7
+        } else {
+            defaultBoton.alpha = 1.0
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let firstTouch = touches.first else { return }
+        let touch: UITouch = firstTouch
+        let contactPosition: CGPoint = touch.location(in: self)
+        
+        if defaultBoton.contains(contactPosition) {
+            actionable(indexer)
+        }
+        defaultBoton.alpha = 1.0
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        defaultBoton.alpha = 1.0
+    }
 }
