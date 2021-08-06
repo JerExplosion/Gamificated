a5e00b547b773049f7aa54002d8eab4e6b978b7f
jer .explosion <jerryren.ios@gmail.com>
2021-08-05 23:05:12 -0400

    Adopting KolaViewCon as super-class

---
+++ SequentialColorful/SequentialColorful/ViewControllers/KolaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  KolaViewController.swift
+//  SequentialColorful
+//
+//  Created by Jerry Ren on 8/5/21.
+//
+
+import UIKit
+
+class KolaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
