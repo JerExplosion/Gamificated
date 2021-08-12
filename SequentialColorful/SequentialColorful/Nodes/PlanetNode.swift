663b879ef0335005a4def5930ad3736989fc96bf
jer .explosion <jerryren.ios@gmail.com>
2021-08-11 22:55:56 -0400

    Setting planet-node's spherical geometry & firstMaterial

--- SequentialColorful/SequentialColorful/Nodes/PlanetNode.swift
+++ SequentialColorful/SequentialColorful/Nodes/PlanetNode.swift
@@ -10,6 +10,10 @@ import SceneKit
 class PlanetNode: SCNNode {
     override init() {
         super.init()
+        
+        self.geometry = SCNSphere(radius: 1.0)
+        self.geometry?.firstMaterial?.transparency = 1
+        self.geometry?.firstMaterial?.shininess = 77
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
